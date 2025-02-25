4fb6705da1496a550fbcc8dd6122571b0550ed41
Tiiickk <fabian.cange@hotmail.fr>
2025-02-25 11:11:12 +0100

    cart article // cart

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './ProductCard.css'
+
+export default function ProductCard ({ product }) {
+  return (
+    <div className="product-card">
+      <img src={product.image} alt={product.name} />
+      <h3>{product.name}</h3>
+      <p>{product.price}€</p>
+      <Link to={`/product/${product.id}`} className="details-btn">Voir Détails</Link>
+    </div>
+  )
+}
